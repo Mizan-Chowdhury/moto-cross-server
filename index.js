6fa7786394bedb515a86b4ebe00ba39d1cb64668
mizan-chowdhury <mizan1034@gmail.com>
2023-10-20 19:50:20 +0600

    server site has deployed on vercel

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     const productCollection = client.db("motoCrossDB").collection("products");
     const cartCollection = client.db("motoCrossDB").collection("cartProduct");
 
@@ -88,7 +88,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
